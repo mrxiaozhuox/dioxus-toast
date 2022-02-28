3106890d4a9b07fc6e555b1d3bb13b93b692d442
YuKun Liu <41265098+mrxiaozhuox@users.noreply.github.com>
2022-02-28 22:13:28 +0800

    feat: commit code

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,5 +1,5 @@
 use dioxus::prelude::*;
-use dioxus_toast::{ToastManager, ToastInfo};
+use dioxus_toast::ToastManager;
 
 fn main() {
     dioxus::desktop::launch(app)
@@ -14,15 +14,15 @@ fn app(cx: Scope) -> Element {
     let toast = use_ref(&cx, ToastManager::default);
 
     cx.render(rsx! {
-        dioxus_toast::Toast {
+        dioxus_toast::ToastFrame {
             manager: toast
         }
         div {
             button {
                 onclick: move |_| {
-                    toast.write().popup(ToastInfo {
-                        text: "hello world".into(),
-                    });
+                    toast.write().popup(cx.render(rsx! {
+                        div { "123" }
+                    }));
                 },
                 "弹出"
             }
--- src/lib.rs
+++ src/lib.rs
@@ -8,32 +8,45 @@ use dioxus::prelude::*;
 use uuid::Uuid;
 
 #[derive(Default, Debug)]
-pub struct ToastManager {
-    list: HashMap<Uuid, ToastInfo>,
+pub struct ToastManager<'a> {
+    list: HashMap<Uuid, Element<'a>>,
 }
 
-impl ToastManager {
-    pub fn popup(&mut self, option: ToastInfo) -> Uuid {
+impl<'a> ToastManager<'a> {
+    pub fn popup(&mut self, option: Element<'a>) -> Uuid {
         let uuid = Uuid::new_v4();
         self.list.insert(uuid, option);
         uuid
     }
 }
 
-#[derive(Clone, Debug)]
-pub struct ToastInfo {
-    pub text: String
+#[derive(Props)]
+pub struct ToastProps<'a> {
+    text: &'a str
+}
+
+pub fn Toast<'a>(cx: Scope<'a, ToastProps<'a>>) -> Element {
+    cx.render(rsx! {
+        div {}
+    })
 }
 
 #[inline_props]
-pub fn Toast<'a>(cx: Scope, manager: &'a UseRef<ToastManager>) -> Element {
+pub fn ToastFrame<'a>(cx: Scope, manager: &'a UseRef<ToastManager<'a>>) -> Element {
 
     println!("{:?}", manager.read());
 
     cx.render(rsx! {
         div {
             class: "toast-scope",
-            style { [ include_str!("./assets/toast.css") ] }
+            style { [ include_str!("./assets/toast.css") ] },
+            div {
+                class: "toast-wrap",
+                div {
+                    class: "toast-single",
+                    "123"
+                }
+            }
         }
     })
 }
